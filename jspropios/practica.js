2997fd6afc19522c84e90aeec3eb510e723a3841
francoraniolo <franco11-09@hotmail.com>
2018-04-05 13:36:37 -0300

    probando strip table

--- jspropios/practica.js
+++ jspropios/practica.js
@@ -11,8 +11,7 @@ $(function(){
         $("#listarEquipos").click(function(){
             $("#carouselNews").hide();
             var equipos=JSON.parse($.getJSON({'url': "../json/equipos.json", 'async': false}).responseText);
-            var equipos=JSON.parse($.getJSON({'url': "../json/equipos.json", 'async': false}).responseText);
-            $.alert(equipos[0].nombre);
+            
          
         });
             
