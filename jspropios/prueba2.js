213aa1cf3b67016d7a1cfb5ca528e94d417ed540
francoraniolo <franco11-09@hotmail.com>
2018-04-05 13:44:20 -0300

    ups

--- jspropios/prueba2.js
+++ jspropios/prueba2.js
@@ -11,7 +11,7 @@ $(function(){
             $("#carouselNews").hide();
            $.get("json/equipos.json",function(data){
                for(i=0;i<data.length;i++){
-                $("#listaDeEquipos").append($("<td></td>").text(data[i].nombre));
+                $("#listaDeEquipos").append($("<tr></tr>").text(data[i].nombre));
                }
 
                $("#listaDeEquipos").show();
